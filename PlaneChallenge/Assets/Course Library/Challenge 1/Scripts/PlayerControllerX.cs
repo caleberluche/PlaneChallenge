02b1c5a800e20091f95b7e37fea8bddeee89d515
caleberluche <berluchec@roverkids.org>
2024-11-22 13:27:54 -0500

    finished

--- PlaneChallenge/Assets/Course Library/Challenge 1/Scripts/PlayerControllerX.cs
+++ PlaneChallenge/Assets/Course Library/Challenge 1/Scripts/PlayerControllerX.cs
@@ -7,7 +7,6 @@ public class PlayerControllerX : MonoBehaviour
     public float speed;
     public float rotationSpeed;
     public float verticalInput;
-    public float horizontalInput;
 
     // Start is called before the first frame update
     void Start()
@@ -20,12 +19,11 @@ public class PlayerControllerX : MonoBehaviour
     {
         // get the user's vertical input
         verticalInput = Input.GetAxis("Vertical");
-        horizontalInput = Input.GetAxis("Horizontal")
 
         // move the plane forward at a constant rate
         transform.Translate(Vector3.forward * speed * Time.deltaTime);
 
         // tilt the plane up/down based on up/down arrow keys
-        transform.Rotate(Vector3.right * rotationSpeed *  verticalInput * Time.deltaTime);
+        transform.Rotate(Vector3.right * rotationSpeed * verticalInput * Time.deltaTime);
     }
 }
